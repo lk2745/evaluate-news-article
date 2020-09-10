ed2719ed771a14f4ca46aad435ced9272a7a361d
lk2745 <69129636+lk2745@users.noreply.github.com>
2020-09-10 12:12:05 -0400

    Updated formHandler and Index.js

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,15 +1,43 @@
+  
+  /* Function to POST data */
+  const postData = async (url = "", data = {}) => {
+    console.log("postData Function running", data);
+    const response = await fetch(url, {
+      method: "POST",
+      credentials: "same-origin",
+      mode: 'no-cors',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data)
+    });
+    try {
+      const newData = await response.json();
+      return newData;
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+
 function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
+    let formText = document.getElementById('url').value
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    
+    postData('http://localhost:8081/meaningCloud', {url: formText})
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+        const getData = Object.getData(res);
+        document.getElementById('polarity').innerHTML = `Polarity: ${getData.score_tag}`;
+        document.getElementById("agreement").innerHTML = `Agreement: ${getData.agreement}`;
+        document.getElementById("subjectivity").innerHTML = `Subjectivity: ${getData.subjectivity}`;
+        document.getElementById("confidence").innerHTML = `Confidence: ${getData.confidence}`;
+        document.getElementById("irony").innerHTML = `Irony: ${getData.irony}`;
     })
 }
 
--- src/server/index.js
+++ src/server/index.js
@@ -1,33 +1,61 @@
+//SERVER SIDE
 const dotenv = require('dotenv');
 dotenv.config();
 
 var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
-
+const apiKey = process.env.API_KEY
 console.log(`Your API Key is $process.env.API_KEY`);
+let entryUrl = [];
+const fetch = require('node-fetch');
 
-// You could call it meaningcloudapi, or anything else
-var textapi = new meaningcloud({
-    application_key: process.env.API_KEY
-});
-     
+const express = require('express')
+var bodyParser = require('body-parser')
+const mockAPIResponse = require('./mockAPI.js')
 const app = express()
 
+// Cors for cross origin allowance - allows the browser and server to communicate without any security interruptions
+const cors = require('cors');
+
+const e = require('express');
+app.use(cors());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
 app.use(express.static('dist'))
 
 console.log(__dirname)
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
+    //res.sendFile('dist/index.html')
     res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
-    console.log('Example app listening on port 8081!')
+    console.log('MeaningCloud app listening on port 8081!')
 })
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+
+
+// Post Route
+app.post('/meaningCloud', addMeaning);
+    async function addMeaning(req, res) {
+        entryUrl = req.body.url;
+        console.log("The URL entered:", entryUrl);
+        const apiUrl = `https://api.meaningcloud.com/sentiment-2.1?key=${apiKey}&url=${req.body.url}&lang=en`
+
+        const response = await fetch(apiUrl)
+        const meaningdata = await response.json()
+        const projectData = {
+            scoretag : meaningdata.score_tag,
+            agreement : meaningdata.agreement,
+            confidence : meaningdata.confidence,
+            irony : meaningdata.irony
+        }
+        console.log(projectData);
+        res.send(projectData);
+    }
